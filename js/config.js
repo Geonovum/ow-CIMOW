3716b48c90c720adb7b16d51d5bdeb275c96f9a9
Geonovum <>
2022-06-03 15:50:20 +0000

    Update from https://github.com/Geonovum/word2respec_publicatie/commit/8869a2e70584c1899c790df1dc889cb267a88b42

---
+++ js/config.js
@@ -0,0 +1,28 @@
+var respecConfig =
+{
+  pubDomain: "ow",
+  specStatus: "GN-WV",
+  specType: "IM",
+  license: "cc-by-nd",
+  publishDate: "2022-06-03",
+  authors: [
+    {
+      name: "DSO project 33",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Wilko Quak",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "CIMOW",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ow-CIMOW/",
+  issueBase: "https://github.com/Geonovum/ow-CIMOW/issues/",
+  edDraftURI: "https://Geonovum.github.io/ow-CIMOW/",
+  generatedSubtitle: "Werkversie 3 juni 2022"
+}
